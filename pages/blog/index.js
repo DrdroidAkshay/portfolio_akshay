83f0ae86b77fbf42f77e2536a96f9aea86f43adf
Akshay <akshayyadav04031998@gmail.com>
2022-02-11 23:33:22 +0530

    navigating using Link

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,25 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, UnorderedList, ListItem} from "@chakra-ui/react";
+import Link from "next/link";
 
+const list = [{ title: "Blog1", path: 1 }, { title :"Blog2", path:2}, { title :"Blog3", path:3}];
 const Blog = () => {
-  return <Box>Blog</Box>;
+  const renderListItem = (
+    listItem
+    )=>{
+    return (
+      <Link href={`/blog/${listItem.path}`} passHref>
+        <ListItem>{listItem.title}</ListItem>
+      </Link>
+    );
+  };
+  return (
+    <Box>
+      <Heading>Blog</Heading>
+      <UnorderedList>
+        {list.map(renderListItem)}
+      </UnorderedList>
+    </Box>
+  );
 };
 
 export default Blog;
